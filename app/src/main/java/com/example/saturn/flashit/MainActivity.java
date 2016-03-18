fd8c68d9d75ca41bfd16786599fa41e48fa54d30
arpitagarwal1301 <arpitvinshu@gmail.com>
2016-03-19 04:34:58 +0530

    Toggle Button used instead of ImageButton Flickering logic corrected New Button images used

--- app/src/main/java/com/example/saturn/flashit/MainActivity.java
+++ app/src/main/java/com/example/saturn/flashit/MainActivity.java
@@ -8,17 +8,21 @@ import android.os.Bundle;
 import android.test.AndroidTestRunner;
 import android.util.Log;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.ImageButton;
+import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import java.security.spec.ECField;
 
 
 public class MainActivity extends Activity {
 
-    private ImageButton button;
+    //private ImageButton button;
+    private ToggleButton button;
     private  boolean isFlashOn=false;
     private Camera camera;
     private SeekBar seekBar;
@@ -51,8 +55,8 @@ public class MainActivity extends Activity {
         textView = (TextView)findViewById(R.id.textView_progress);
         seekBar = (SeekBar)findViewById(R.id.seekBar);
 
-        /*Button for switching ON/OFF the torch*/
-        button = (ImageButton)findViewById(R.id.imageButton);
+      /*  *//*Button for switching ON/OFF the torch*//*
+        button = (Button)findViewById(R.id.imageButton);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -75,8 +79,35 @@ public class MainActivity extends Activity {
                     stopFlicker=false;
                 }
             }
-        });
+        });*/
 
+        //Using toggle button with background image
+        button = (ToggleButton)findViewById(R.id.imageButton);
+        button.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                isFlashOn= isChecked;
+                if (!isChecked) {
+                    // The toggle is enabled
+                    Log.i("info", "torch is turned off!");
+                    p.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+                    camera.setParameters(p);
+                    isFlashOn = false;
+                    seekBar.setProgress(0);
+                    textView.setText(0 + "");
+                    if (t != null) {
+                        stopFlicker = true;
+                        t = null;
+                    }
+                } else {
+                    // The toggle is disabled
+                    Log.i("info", "torch is turned on!");
+                    p.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+                    camera.setParameters(p);
+                    isFlashOn = true;
+                    stopFlicker=false;
+                }
+            }
+        });
 
        SeekBarMethod();
     }
@@ -104,12 +135,11 @@ public class MainActivity extends Activity {
             public void onStopTrackingTouch(SeekBar seekBar) {
                 textView.setText(progressValue + "");
                 flashFlicker();
-
             }
 
             private void flashFlicker(){
                 if(isFlashOn){
-                    freq = Math.abs(seekBar.getProgress());
+                    freq = seekBar.getProgress();
                     stroboRunner = new StroboRunner();
                     t = new Thread(stroboRunner);
                     t.start();
@@ -132,12 +162,19 @@ public class MainActivity extends Activity {
             try{
                 while(!stopFlicker){
                     final Camera.Parameters p = camera.getParameters();
-                    p.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
-                    camera.setParameters(p);
-                    Thread.sleep(10 - freq);
-                    p.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
-                    camera.setParameters(p);
-                    Thread.sleep(freq);
+                    if(freq!=0){
+                        p.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+                        camera.setParameters(p);
+                        Thread.sleep((long)Math.floor(freq*100));
+                        p.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+                        camera.setParameters(p);
+                        // Thread.sleep(freq);
+                        Thread.sleep((long)Math.floor(freq*100));
+                    }else{
+                        p.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+                        camera.setParameters(p);
+                    }
+
                 }
 
             }catch (Exception e){
